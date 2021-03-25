4fad8f6e81f8ce1bc696c262ba88340637cb3fb0
Francis <francismistica06@gmail.com>
2021-03-25 12:55:50 +0800

    Added a script file to index

---
+++ app.js
@@ -0,0 +1,6 @@
+let hide = document.querySelector('.container');
+let user = 'Kiko915';
+
+hide.addEventListener('click', function() {
+	alert('Congratulations ' + user + ' at your first repo and successful commits!');
+})
